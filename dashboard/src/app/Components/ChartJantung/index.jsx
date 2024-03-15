58355bccab4e7648db9f8d2d8c070072f03f1637
FarhanWijonarko <farhan.wijonarko1@gmail.com>
2024-03-15 21:17:00 +0700

    membuat chartJantung

--- dashboard/src/app/Components/ChartJantung/index.jsx
+++ dashboard/src/app/Components/ChartJantung/index.jsx
@@ -6,119 +6,44 @@ import { useEffect, useState } from 'react';
 
 const ChartJantung = () => {
   const [data, setData] = useState({
-    series: [
-      {
-        data: [],
-      },
-    ],
-    options: {
-      chart: {
-        height: 350,
-        type: 'line',
-        zoom: {
-          enabled: false,
-        },
-      },
-      dataLabels: {
-        enabled: false,
-      },
-      stroke: {
-        curve: 'straight',
-      },
-      title: {
-        text: 'Heart Rate',
-        align: 'left',
-      },
-      grid: {
-        row: {
-          colors: ['#f3f3f3', 'transparent'],
-          opacity: 0.5,
-        },
-      },
-      xaxis: {
-        categories: [],
-      },
+    chart: {
+      id: 'apexchart-example',
+    },
+    xaxis: {
+      categories: ['1', '2', '3', '4', '5', '6', '7', '8', '9', '10', '11', '12', '13', '14', '15', '16', '17', '18', '19', '20', '21', '22', '23', '24'],
     },
   });
+  const [series, setSeries] = useState([
+    {
+      name: 'Heart Rate',
+      data: [],
+    },
+  ]);
   useEffect(() => {
     const data = [];
-    const jam = [];
-    const fetchData = async () => {
-      try {
-        const response = await fetch('https://hias1v0.localto.net/TA-PROJECT/backend_laravel/public/api/patients', { mode: 'cors' });
-        if (!response.ok) {
-          throw new Error('Network response was not ok');
-        }
-        const responseData = await response.json();
-        console.log(responseData);
-        const seriesData = responseData.map((item) => item.heartrate);
-        const categories = responseData.map((item) => item.jam);
-        setData((prevData) => ({
-          ...prevData,
-          series: [{ data: seriesData }],
-          options: {
-            ...prevData.options,
-            xaxis: { categories },
-          },
-        }));
-      } catch (error) {
-        console.error('Error fetching data:', error);
-      }
-    };
-
-    fetchData();
-    // axios
-    //   .get('https://hias1v0.localto.net/TA-PROJECT/backend_laravel/public/api/patients')
-    //   .then((res) => {
-    //     console.log(res);
-    //     data.map((item) => {
-    //       console.log(item.data.heartrate.heart_beats);
-    //       data.push(item.data.heartrate.heart_beats);
-    //     });
-    //     setData({
-    //       series: [
-    //         {
-    //           data: [data],
-    //         },
-    //       ],
-    //       options: {
-    //         chart: {
-    //           height: 350,
-    //           type: 'line',
-    //           zoom: {
-    //             enabled: false,
-    //           },
-    //         },
-    //         dataLabels: {
-    //           enabled: false,
-    //         },
-    //         stroke: {
-    //           curve: 'straight',
-    //         },
-    //         title: {
-    //           text: 'Heart Rate',
-    //           align: 'left',
-    //         },
-    //         grid: {
-    //           row: {
-    //             colors: ['#f3f3f3', 'transparent'],
-    //             opacity: 0.5,
-    //           },
-    //         },
-    //         xaxis: {
-    //           categories: [jam],
-    //         },
-    //       },
-    //     });
-    //   })
-    //   .catch((err) => {
-    //     console.log(err);
-    //   });
+    axios.get('http://192.168.18.8:8080/TUGAS-AKHIR/backend_laravel/public/api/patients').then((response) => console.log(response.data));
+    response.map((response) => {
+      data.push(response.heart_rate);
+    });
+    setData({
+      chart: {
+        id: 'apexchart-example',
+      },
+      xaxis: {
+        categories: ['1', '2', '3', '4', '5', '6', '7', '8', '9', '10', '11', '12', '13', '14', '15', '16', '17', '18', '19', '20', '21', '22', '23', '24'],
+      },
+    });
+    setSeries([
+      {
+        name: 'Heart Rate',
+        data: data,
+      },
+    ]).catch((error) => console.log(error));
   }, []);
   return (
     <div className="bg-white h-[44vh] rounded-lg shadow-lg w-[60vh] p-5 ml-10">
       <div id="chart">
-        <ReactApexChart options={data.options} series={data.series} type="area" height={350} />
+        <ReactApexChart options={data} series={series} type="line" height={350} />
       </div>
     </div>
   );
